f6458de82158fa045eff8730b9384688a08975c7
Steve Harris (drstevok-test1) <git@steveharris.me>
2016-01-12 11:02:27 +0000

    added occupancy rmarkdown

---
+++ write/5_results_05occupancy.R
@@ -0,0 +1,13 @@
+# Uncomment when coding
+# rm(list=ls(all=TRUE))
+# source("write/5_results_01start.R")
+require(assertthat)
+assert_that("wdt" %in% ls())
+
+lookfor("room")
+describe(wdt$room_cmp2)
+(room <- ff.np('room_cmp2', wdt[!is.na(room_cmp2)], dp=0))
+
+
+
+
